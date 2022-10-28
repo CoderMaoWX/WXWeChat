555906ad9b5b1b86e3c7e22ac568111b04a951be
maowangxin <maowangxin@zaful.com>
2022-10-28 14:33:12 +0800

    Initial Commit

---
+++ WXWeChat/WXWeChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WXWeChatApp.swift
+//  WXWeChat
+//
+//  Created by 610582 on 2022/10/28.
+//
+
+import SwiftUI
+
+@main
+struct WXWeChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
